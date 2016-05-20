94ebfbedffb2cd397b51ecf97a5ec1e2e7e561d9
Kelsea <andersonkelsea@gmail.com>
2016-05-20 18:50:19 -0400

    Beginning of node shopping list

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+var Storage = function() {
+    this.items = [];
+    this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+    var item = {name: name, id: this.id};
+    this.items.push(item);
+    this.id += 1;
+    return item;
+};
+
+var storage = new Storage();
+storage.add('Broad beans');
+storage.add('Tomatoes');
+storage.add('Peppers');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/items', function(req, res) {
+    res.json(storage.items);
+});
+
+app.post('/items', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+
+    var item = storage.add(req.body.name);
+    res.status(201).json(item);
+});
+
+app.delete('/items/:id', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+    else{
+        var item = storage.remove(req.params.id);
+        return res.status(201).json(item);
+    }
+});
+
+app.put('/items/:id', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+    else{
+        var item = storage.update(req.params.id, req.body.name, function(item){
+        return res.status(200).json(item);
+        });
+    }
+});
+
+app.listen(process.env.PORT || 8080);
+
+exports.app = app;
+exports.storage = storage;
